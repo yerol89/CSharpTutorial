b198dd4d407cacd3180fed712b5bd7bbc270a704
Yunus Talip Erol <190706012@st.maltepe.edu.tr>
2021-02-02 14:06:22 +0300

    Proje dosyası ekle.

---
+++ Operators/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OPERATING ON VARIABLES
+            //Operators apply simple operations such as addition and multiplication to operands such as variables and literal values.
+            int x = 5; //Declared an integer
+            int incrementedByOne = x++; //Incremented value of x by 1
+            Type theTypeOfAnInteger = typeof(int); // Determine the type of a variable
+            int howManyBytesInAnInteger = sizeof(int); // Determine the size of a variable
+            // Increment by POSTFIX
+            // The output will be a=4 and b=3
+            //Why not b is equal to 4?
+            //Because the '++' operator which is used AFTER the variable makes the increment after the assignment.
+            int a = 3;
+            int b = a++;
+            Console.WriteLine($"a is {a}, b is {b}");
+            Console.WriteLine("************************************************************************");
+            // Increment by PREFIX
+            // The output will be c=7 and d=7
+            //In this example'++' operator which is used BEFORE the variable makes the increment BEFORE the assignment.
+            int c = 6;
+            int d = ++c;
+            Console.WriteLine($"c is {c}, d is {d}");
+            Console.WriteLine("************************************************************************");
+            //Arithmetic Operators
+            int e = 11;
+            int f = 3;
+            Console.WriteLine($"e is {e}, f is {f}");
+            Console.WriteLine($"e + f = {e + f}");
+            Console.WriteLine($"e - f = {e - f}");
+            Console.WriteLine($"e * f = {e * f}");
+            Console.WriteLine($"e / f = {e / f}"); // Gives the integer value of division,if define e or f as double or float it gives the exact result
+            Console.WriteLine($"e % f = {e % f}"); // Gives the remainder from the division of these integers
+            Console.WriteLine("************************************************************************");
+            //Assignment Operators
+            int p = 6;
+            p += 3; // equivalent to p = p + 3;
+            p -= 3; // equivalent to p = p - 3;
+            p *= 3; // equivalent to p = p * 3;
+            p /= 3; // equivalent to p = p / 3;
+            Console.WriteLine("************************************************************************");
+            //Logical Operators
+            bool b1 = true;
+            bool b2 = false;
+            Console.WriteLine($"AND | b1 | b2 ");
+            Console.WriteLine($"b1 | {b1 && b1,-5} | {b1 && b2,-5} ");
+            Console.WriteLine($"b2 | {b2 && b1,-5} | {b2 && b2,-5} ");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine($"OR | b1 | b2 ");
+            Console.WriteLine($"b1 | {b1 || b1,-5} | {b1 || b2,-5} ");
+            Console.WriteLine($"b2 | {b2 || b1,-5} | {b2 || b2,-5} ");
+            Console.WriteLine("************************************************************************");
+            //This example very useful for be aware of another type of operands
+            // How many operators in the following statement?
+            int age = 47;
+            // How many operators in the following statement?
+            string convertString = age.ToString();
+            char firstDigit = convertString[0];
+            Console.WriteLine($"The first character of age is {firstDigit}");
+            // There are four operators:
+            // = is the assignment operator
+            // . is the member access operator
+            // () is the invocation operator
+            // [] is the indexer access operator
+        }
+    }
+}
---
+++ SpeakingCSharp/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace SpeakingCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //We assigned 1.88 value to VARIABLE 'heightInMetres'
+            double heightInMetres = 1.95;
+            Console.WriteLine($"The variable {nameof(heightInMetres)} has the value { heightInMetres}.");
+            Console.WriteLine("************************************************************************");
+            //A literal value represents a fixed value
+            char myLetter = 'Y'; // assigning literal characters
+            string lastName = "Erol"; // assigning literal string
+
+
+            // WORKING WITH NUMBERS: Mostly used number variables are int, float and double
+            // Let's have a look at their size and min. to max. value
+            Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range { int.MinValue:N0} to { int.MaxValue:N0}.");
+            Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range { double.MinValue:N0} to {double.MaxValue:N0}.");
+            Console.WriteLine($"float uses {sizeof(float)} bytes and can store numbers in the range { float.MinValue:N0} to { float.MaxValue:N0}.");
+            Console.WriteLine("************************************************************************");
+            // Here are some variable types 
+            int population = 85000000; // 66 million in UK
+            double weight = 1.76; // in kilograms
+            string fruit = "Bananas"; // strings use double-quotes
+            char letter = 'Y'; // chars use single-quotes
+            bool areYouHappy = true; // Booleans have value of true or false
+
+            // We can also use 'var' keyword to define a variable. The compiler will understand if it is an integer, boolean or etc. accordingly to assigned value
+            var population2 = 66_000_000; // 66 million in UK
+            var weight2 = 1.88; // in kilograms
+            var fruit2 = "Apples"; // strings use double-quotes
+            var letter2 = 'Z'; // chars use single-quotes
+            var areYouHappy2 = true; // Booleans have value of true or false
+
+            //These primitive types(int, double, boolean etc.) are 'value types' except 'string'. Because 'string' is a 'Reference Type'
+            //Reference type means, variable contains the memory adress of a value, not the value itself. 
+            //Reference type may have a 'null' value which means the variable does not reference anything for now. 'null' is the default value of all reference types.
+            //Now let's have a look at the default values of some primitive types  
+            Console.WriteLine($"Default value of int = {default(int)}");
+            Console.WriteLine($"Default value of boolean = {default(bool)}");
+            Console.WriteLine($"Default value of DateTime = {default(DateTime)}");
+            Console.WriteLine($"Default value of string = {default(string)}");
+            Console.WriteLine("************************************************************************");
+            //What if we want to store multiple values of same variable type? We have 'array' for this.
+            //Let's assume we need store name of family members 
+            string[] familyMembers; // This statement is an array reference
+            familyMembers = new string[3]; //Now we allocated memory for this reference, what we should notice here is the '4' inside brackets. 
+            //This is the size of the array. So array is a 'fixed-sized' structure.
+            familyMembers[0] = "Yunus";
+            familyMembers[1] = "Seda";
+            familyMembers[2] = "Almila";
+            Console.WriteLine($"Name of first family member: {familyMembers[0]} ");
+            Console.WriteLine($"Name of second family member: {familyMembers[1]} ");
+            Console.WriteLine($"Name of third family member: {familyMembers[2]} ");
+            Console.WriteLine("************************************************************************");
+            //GETTING INPUT FROM USER
+            //We use 'Readline' method for getting input from user
+            Console.WriteLine("What is your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine($"You are {name} and {age} years old");
+        }
+    }
+}
