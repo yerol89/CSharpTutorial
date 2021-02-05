141a1fdd9cbf6f2c8e90e045f7d8b6c121534a66
Yunus Talip Erol <190706012@st.maltepe.edu.tr>
2021-02-05 12:38:26 +0300

    C#

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // IF STATEMENTS
+            //if (expression1)
+            //{
+            //    // runs if expression1 is true
+            //}
+            //else if (expression2)
+            //{
+            //    // runs if expression1 is false and expression2 if true
+            //}
+            //else if (expression3)
+            //{
+            //    // runs if expression1 and expression2 are false
+            //    // and expression3 is true
+            //}
+            //else
+            //{
+            //    // runs if all expressions are false
+            //}
+            int x = 20;
+            int y = 40;
+            if (x > y)
+            {
+                Console.WriteLine("x is greater than y");
+            }
+            else if (x == y)
+            {
+                Console.WriteLine("x is equal to y");
+            }
+            else
+            {
+                Console.WriteLine("y is greater than x");
+            }
+
+            //SWITCH STATEMENT
+            //switch (expression)
+            //{
+            //    case x:
+            //        // code block
+            //        break;
+            //    case y:
+            //        // code block
+            //        break;
+            //    default:
+            //        // code block
+            //        break;
+            //}
+
+            int day = 4;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Wrong Choice!!!");
+                    break;
+            }
+        }
+    }
+}
