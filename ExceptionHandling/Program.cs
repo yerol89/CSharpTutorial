21481e0856e113d4ffd3e336cbe4e39804cbe809
Yunus Talip Erol <190706012@st.maltepe.edu.tr>
2021-02-05 19:51:18 +0300

    ExceptionHandling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Convert;
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //If the user tries to input 'string' type instead of 'double' s/he will get an error
+            try
+            {
+                double number1, number2;
+                double result;
+                Console.WriteLine("Input a number: ");
+                number1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Input a number: ");
+                number2 = Convert.ToDouble(Console.ReadLine());
+                result = ToDouble(number1 / number2);
+                Console.WriteLine($"Division of {number1} and {number2} is {result} ");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"{ex.GetType()} says {ex.Message}");
+            }
+           
+        }
+    }
+}
